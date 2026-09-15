6493d52e738a3e0dd0dd352e4e07e57a5e63aceb
roog23 <roog2320@gmail.com>
2026-09-15 14:56:01 +0900

    feat : 더미데이터 생성 수정

--- src/test/java/com/example/oneplusone/domain/product/service/InitProductsTest.java
+++ src/test/java/com/example/oneplusone/domain/product/service/InitProductsTest.java
@@ -27,10 +27,42 @@ public class InitProductsTest {
         User saveUser = userRepository.save(user);
         List<Product> productList = new ArrayList<>();
         for(int i = 1; i <= 50000; i ++){
-            String name = "product" + i;
-            Long price = 10000L + i;
+            String name;
+
+            if (i % 10 == 0) {
+                name = "special product" + i;
+            } else if (i % 10 == 1) {
+                name = "good product" + i;
+            } else {
+                name = "product" + i;
+            }
+
+            String type;
+            if (i % 10 == 0) {
+                type = "과일";
+            } else if (i % 10 == 1) {
+                type = "음료";
+            } else if (i % 10 == 2) {
+                type = "전자기기";
+            }else if (i % 10 == 3) {
+                type = "생필품";
+            }else if (i % 10 == 4) {
+                type = "냉동식품";
+            }else if (i % 10 == 5) {
+                type = "냉장식품";
+            }else if (i % 10 == 6) {
+                type = "건조식품";
+            }else if (i % 10 == 7) {
+                type = "필기도구";
+            }else if (i % 10 == 8) {
+                type = "청소용품";
+            }else {
+                type = "과자";
+            }
+
+            Long price = 1000L + (i % 9001);
             Long quantity = 1L + i;
-            Product product = new Product(name, "type", price, quantity, saveUser);
+            Product product = new Product(name, type, price, quantity, saveUser);
             productList.add(product);
         }
         productRepository.saveAll(productList);
